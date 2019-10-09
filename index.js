9d5e6cde57edf3c28e9abfbec6e95434c4281c35
AJ <akinjcampbell@gmail.com>
2019-10-09 12:10:00 -0500

    Summary
    Complete step 4:
    -add a service worker for caching
    source: https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker

---
+++ index.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/css/style.css',
+          '/css/main.css',
+          '/js/main.js',
+          '/js/dbhelper.js',
+          '/js/restaurant_info.js',
+          '/index.html',
+          '/restaurant.html'
+        ]
+      );
+    })
+  );
+});
